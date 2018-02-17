415311e97d90451d6cef4ce35f6f3be31a766c22
Cẩm Huỳnh <huynhquancam@gmail.com>
2018-02-17 10:47:31 +0100

    Add Travis

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Saxy.MixProject do
     [
       app: :saxy,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
