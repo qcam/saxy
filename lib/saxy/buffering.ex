6cf716315fa1d7d273017cedc5c4c979c1f3d38d
Cẩm Huỳnh <huynhquancam@gmail.com>
2018-08-04 18:53:09 +0200

    Simplify context function in buffering

--- lib/saxy/buffering.ex
+++ lib/saxy/buffering.ex
@@ -3,7 +3,7 @@ defmodule Saxy.Buffering do
 
   defmacro buffering_parse_fun(fun_name, arity, token \\ "")
   defmacro buffering_parse_fun(fun_name, arity, token) do
-    quoted_params =
+    params_splice =
       case arity do
         5 -> quote(do: [cont, original, pos, state])
         6 -> quote(do: [cont, original, pos, state, acc1])
@@ -12,57 +12,58 @@ defmodule Saxy.Buffering do
         9 -> quote(do: [cont, original, pos, state, acc1, acc2, acc3, acc4])
         10 -> quote(do: [cont, original, pos, state, acc1, acc2, acc3, acc4, acc5])
       end
-    quoted_fun =
+
+    context_fun =
       case arity do
-        5 -> quote(do: &(unquote(fun_name)(&1, &2, &3, &4, &5)))
-        6 -> quote(do: &(unquote(fun_name)(&1, &2, &3, &4, &5, acc1)))
-        7 -> quote(do: &(unquote(fun_name)(&1, &2, &3, &4, &5, acc1, acc2)))
-        8 -> quote(do: &(unquote(fun_name)(&1, &2, &3, &4, &5, acc1, acc2, acc3)))
-        9 -> quote(do: &(unquote(fun_name)(&1, &2, &3, &4, &5, acc1, acc2, acc3, acc4)))
-        10 -> quote(do: &(unquote(fun_name)(&1, &2, &3, &4, &5, acc1, acc2, acc3, acc4, acc5)))
+        5 -> quote(do: &(unquote(fun_name)(&1, &2, &3, pos, state)))
+        6 -> quote(do: &(unquote(fun_name)(&1, &2, &3, pos, state, acc1)))
+        7 -> quote(do: &(unquote(fun_name)(&1, &2, &3, pos, state, acc1, acc2)))
+        8 -> quote(do: &(unquote(fun_name)(&1, &2, &3, pos, state, acc1, acc2, acc3)))
+        9 -> quote(do: &(unquote(fun_name)(&1, &2, &3, pos, state, acc1, acc2, acc3, acc4)))
+        10 -> quote(do: &(unquote(fun_name)(&1, &2, &3, pos, state, acc1, acc2, acc3, acc4, acc5)))
       end
 
     if token == :utf8 do
       quote do
         # 2-byte/3-byte/4-byte unicode
-        def unquote(fun_name)(<<1::size(1), rest::size(7)>>, unquote_splicing(quoted_params))
+        def unquote(fun_name)(<<1::size(1), rest::size(7)>>, unquote_splicing(params_splice))
             when cont != :done do
-          Saxy.Buffering.maybe_buffer(<<1::size(1), rest::size(7)>>, cont, original, pos, state, unquote(quoted_fun))
+          Saxy.Buffering.maybe_buffer(<<1::size(1), rest::size(7)>>, cont, original, unquote(context_fun))
         end
 
         # 3-byte/4-byte unicode
-        def unquote(fun_name)(<<1::size(1), 1::size(1), rest::6-bits, next_char::1-bytes>>, unquote_splicing(quoted_params))
+        def unquote(fun_name)(<<1::size(1), 1::size(1), rest::6-bits, next_char::1-bytes>>, unquote_splicing(params_splice))
             when cont != :done do
-          Saxy.Buffering.maybe_buffer(<<1::size(1), 1::size(1), rest::6-bits, next_char::binary>>, cont, original, pos, state, unquote(quoted_fun))
+          Saxy.Buffering.maybe_buffer(<<1::size(1), 1::size(1), rest::6-bits, next_char::binary>>, cont, original, unquote(context_fun))
         end
 
         # # 4-byte unicode
-        def unquote(fun_name)(<<1::size(1), 1::size(1), 1::size(1), rest::5-bits, next_char::2-bytes>>, unquote_splicing(quoted_params))
+        def unquote(fun_name)(<<1::size(1), 1::size(1), 1::size(1), rest::5-bits, next_char::2-bytes>>, unquote_splicing(params_splice))
             when cont != :done do
-          Saxy.Buffering.maybe_buffer(<<1::size(1), 1::size(1), 1::size(1), rest::5-bits, next_char::binary>>, cont, original, pos, state, unquote(quoted_fun))
+          Saxy.Buffering.maybe_buffer(<<1::size(1), 1::size(1), 1::size(1), rest::5-bits, next_char::binary>>, cont, original, unquote(context_fun))
         end
       end
     else
       quote do
-        def unquote(fun_name)(unquote(token), unquote_splicing(quoted_params))
+        def unquote(fun_name)(unquote(token), unquote_splicing(params_splice))
             when cont != :done do
-          Saxy.Buffering.maybe_buffer(unquote(token), cont, original, pos, state, unquote(quoted_fun))
+          Saxy.Buffering.maybe_buffer(unquote(token), cont, original, unquote(context_fun))
         end
       end
     end
   end
 
-  @compile {:inline, [maybe_buffer: 6]}
+  @compile {:inline, [maybe_buffer: 4]}
 
-  def maybe_buffer(<<buffer::bits>>, cont, original, pos, state, fun) do
+  def maybe_buffer(<<buffer::bits>>, cont, original, context_fun) do
     case do_buffer(cont) do
       :done ->
-        fun.(buffer, :done, original, pos, state)
+        context_fun.(buffer, :done, original)
 
       {:ok, {cont_bytes, next_cont}} ->
         buffer = [buffer | cont_bytes] |> IO.iodata_to_binary()
         original = [original | cont_bytes] |> IO.iodata_to_binary()
-        fun.(buffer, next_cont, original, pos, state)
+        context_fun.(buffer, next_cont, original)
     end
   end
 
