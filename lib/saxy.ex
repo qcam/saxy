ef30b2da5068451644a4c71a3cf3c2feb3ed8031
Nick Levesque <nick@levesque.io>
2019-04-06 18:22:21 -0400

    Taking a stab at implementing partial parsing.

--- lib/saxy.ex
+++ lib/saxy.ex
@@ -186,6 +186,38 @@ defmodule Saxy do
     end
   end
 
+  def parse_partial_init(handler, initial_user_state, options \\ [])
+      when is_atom(handler) do
+    expand_entity = Keyword.get(options, :expand_entity, :keep)
+
+    state = %State{
+      prolog: nil,
+      handler: handler,
+      user_state: initial_user_state,
+      expand_entity: expand_entity
+    }
+
+    case Parser.Prolog.parse(<<>>, true, <<>>, 0, state) do
+      {:halted, fun} -> {:ok, fun}
+      {:error, _reason} = error -> error
+    end
+  end
+
+  def parse_partial(data, context_fun) when is_binary(data) do
+    case context_fun.(data, true) do
+      {:halted, fun} -> {:ok, fun}
+      {:error, _reason} = error -> error
+     end
+  end
+
+  def parse_partial_done(context_fun) do
+    case context_fun.(<<>>, false) do
+      {:ok, state} -> {:ok, state.user_state}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+      
   @doc ~S"""
   Parses XML stream data.
 
