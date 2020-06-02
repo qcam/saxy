8c616cb133f8cf8da60be0ea7cb69e7051ecefcc
Cẩm Huỳnh <huynhquancam@gmail.com>
2020-06-02 12:36:29 +0200

    Speed up XML builder

--- test/saxy/builder_test.exs
+++ test/saxy/builder_test.exs
@@ -30,16 +30,16 @@ defmodule Saxy.BuilderTest do
 
   test "builds datetime" do
     date = ~D[2018-03-01]
-    assert build(date) == {:characters, "2018-03-01"}
+    assert build(date) == "2018-03-01"
 
     time = ~T[20:18:11.023]
-    assert build(time) == {:characters, "20:18:11.023"}
+    assert build(time) == "20:18:11.023"
 
     {:ok, naive_datetime} = NaiveDateTime.new(~D[2018-01-01], ~T[23:04:00.005])
-    assert build(naive_datetime) == {:characters, "2018-01-01T23:04:00.005"}
+    assert build(naive_datetime) == "2018-01-01T23:04:00.005"
 
     datetime = DateTime.utc_now()
-    assert build(datetime) == {:characters, DateTime.to_iso8601(datetime)}
+    assert build(datetime) == DateTime.to_iso8601(datetime)
   end
 
   defmodule Struct do
@@ -54,11 +54,11 @@ defmodule Saxy.BuilderTest do
 
   test "builds element from struct" do
     struct = %Struct{foo: "foo", bar: "bar"}
-    assert build(struct) == {"test", [{"foo", "foo"}], [{:characters, "bar"}]}
+    assert build(struct) == {"test", [{"foo", "foo"}], ["bar"]}
 
     nested_struct = %Struct{bar: struct}
 
-    assert build(nested_struct) == {"test", [{"foo", ""}], [{"test", [{"foo", "foo"}], [{:characters, "bar"}]}]}
+    assert build(nested_struct) == {"test", [{"foo", ""}], [{"test", [{"foo", "foo"}], ["bar"]}]}
 
     underived_struct = %UnderivedStruct{}
     assert_raise Protocol.UndefinedError, fn -> build(underived_struct) end
@@ -68,25 +68,25 @@ defmodule Saxy.BuilderTest do
 
   property "number" do
     check all(integer <- integer()) do
-      assert build(integer) == {:characters, Integer.to_string(integer)}
+      assert build(integer) == Integer.to_string(integer)
     end
 
     check all(float <- float()) do
-      assert build(float) == {:characters, Float.to_string(float)}
+      assert build(float) == Float.to_string(float)
     end
   end
 
   property "bitstring" do
     check all(string <- string(:printable)) do
-      assert build(string) == {:characters, string}
+      assert build(string) == string
     end
   end
 
   property "atom" do
-    assert build(nil) == {:characters, ""}
+    assert build(nil) == ""
 
     check all(atom <- atom(:alphanumeric)) do
-      assert build(atom) == {:characters, Atom.to_string(atom)}
+      assert build(atom) == Atom.to_string(atom)
     end
   end
 end
